5cefbaff53bfc1f5cdd1f7d586921d04a90489cb
melanie971 <melanie.beguer@gmail.com>
2021-02-11 12:37:56 +0100

    adding tests but the last one is not ok

--- test/dappvotingtest.js
+++ test/dappvotingtest.js
@@ -7,10 +7,11 @@ const {
     expectEvent,  // Assertions for emitted events
     expectRevert, // Assertions for transactions that should fail
   } = require('@openzeppelin/test-helpers');
+
 const { expect } = require('chai');
 
 
-// // The solution below does not work I don't get why
+// // The solution below does not work -guess - I did not find out why
 // contract("Voting", function (accounts) {
 
 //     let votingInstance;
@@ -24,7 +25,7 @@ const { expect } = require('chai');
 //     it("Only the admin should be able to start the proposal registration session", async () => {
 
 //        try {
-//             try starting the registrering Proposal using non-admin addresss
+//             await votingInstance.addVoterToList(voter1, {from: owner});
 //             await votingInstance.startRegisteringProposal({from: nonvoter});
 //             assert.isTrue(false);
 //        }
@@ -104,12 +105,113 @@ const { expect } = require('chai');
         await expectRevert.unspecified(votingInstance.addProposal("Proposition2", {from: nonvoter}));
         
         });
+    // a registered voter can submit a prop only after the admin started the session -> to be done
+    it("A proposal is registered by a voter only after session started by admin", async function(){
+        await votingInstance.addVoterToList(voter1, {from: owner});
+        await expectRevert.unspecified(votingInstance.addProposal("Proposition2", {from: voter1}));
+        });
+
+    // a voter cannot vote before the voting session was allowed by adm
+    it("A voter cannot vote before the voting session is allowed by adm", async function(){
+        await votingInstance.addVoterToList(voter1, {from: owner});
+        await votingInstance.startRegisteringProposal({from: owner});
+        await votingInstance.addProposal("Proposition2", {from: voter1});
+        await votingInstance.endOfRegisteringProposal({from: owner});
+        //await votingInstance.startVote({from: owner});
+        await expectRevert.unspecified(votingInstance.DoingTheVote(0, {from: voter1}));
+        
+        });
+
+     // a voter can vote only once
+     it("A voter can vote only once", async function(){
+        await votingInstance.addVoterToList(voter1, {from: owner});
+        await votingInstance.startRegisteringProposal({from: owner});
+        await votingInstance.addProposal("Proposition1", {from: voter1});
+        await votingInstance.endOfRegisteringProposal({from: owner});
+        await votingInstance.startVote({from: owner});
+        await votingInstance.DoingTheVote(0, {from: voter1});
+
+        await expectRevert.unspecified(votingInstance.DoingTheVote(0, {from: voter1}));
+        
+        });
+
+        //a non-voter cannot vote
+        it("A non-voter cannot vote", async function(){
+            await votingInstance.addVoterToList(voter1, {from: owner});
+            await votingInstance.startRegisteringProposal({from: owner});
+            await votingInstance.addProposal("Proposition1", {from: voter1});
+            await votingInstance.endOfRegisteringProposal({from: owner});
+            await votingInstance.startVote({from: owner});
+            await votingInstance.DoingTheVote(0, {from: voter1});
+    
+            await expectRevert.unspecified(votingInstance.DoingTheVote(0, {from: nonvoter}));
+            
+            });
+
+        //a voter cannot vote after admin ends the voting session
+        it("A voter cannot vote after admin ends the session", async function(){
+            await votingInstance.addVoterToList(voter1, {from: owner});
+            await votingInstance.startRegisteringProposal({from: owner});
+            await votingInstance.addProposal("Proposition1", {from: voter1});
+            await votingInstance.endOfRegisteringProposal({from: owner});
+            await votingInstance.startVote({from: owner});
+            await votingInstance.DoingTheVote(0, {from: voter1});
+            await votingInstance.endVote({from: owner});
+    
+            await expectRevert.unspecified(votingInstance.DoingTheVote(0, {from: voter1}));
+            
+            });
+
+            //only the admin should be allowed to end the voting session
+            it("Only the admin can end the voting session", async function(){
+                await votingInstance.addVoterToList(voter1, {from: owner});
+                await votingInstance.startRegisteringProposal({from: owner});
+                await votingInstance.addProposal("Proposition1", {from: voter1});
+                await votingInstance.endOfRegisteringProposal({from: owner});
+                await votingInstance.startVote({from: owner});
+                await votingInstance.DoingTheVote(0, {from: voter1});
+                
+        
+                await expectRevert.unspecified(votingInstance.endVote({from: voter1}));
+                
+                });
+
+            //only the admn can count the votes
+            it("Only the admin can count the votes", async function(){
+                await votingInstance.addVoterToList(voter1, {from: owner});
+                await votingInstance.startRegisteringProposal({from: owner});
+                await votingInstance.addProposal("Proposition1", {from: voter1});
+                await votingInstance.endOfRegisteringProposal({from: owner});
+                await votingInstance.startVote({from: owner});
+                await votingInstance.DoingTheVote(0, {from: voter1});
+                await votingInstance.endVote({from: owner});
+                await expectRevert.unspecified(votingInstance.TheWinnerIs({from: voter1}));
+                
+                });
+
+
+            //a winning proposal is returned -- THIS TEST DOES NOT WORK
+            it("a winning proposal id is returned", async function(){
+                await votingInstance.addVoterToList(voter1, {from: owner});
+                await votingInstance.addVoterToList(voter2, {from: owner});
+                await votingInstance.startRegisteringProposal({from: owner});
+                await votingInstance.addProposal("Proposition1", {from: voter1});
+                await votingInstance.addProposal("Proposition2", {from: voter1});
+                await votingInstance.endOfRegisteringProposal({from: owner});
+                await votingInstance.startVote({from: owner});
+                await votingInstance.DoingTheVote(1, {from: voter1});
+                await votingInstance.DoingTheVote(1, {from: voter2});
+                await votingInstance.endVote({from: owner});
+                await votingInstance.TheWinnerIs({from: owner});
+
+                const winning_prop = await votingInstance.TheWinnerIs({from: owner});
+                expect(winning_prop).to.equal(1);
+                //expect(winning_prop).to.be.equal(1);
+                //assert.equal(winning_prop, 1, "The winning proposal is not the good one");
+                
+                });
 
 
 
-// a registered voter can submit a prop only after the admin started the session -> to be done
-// a voter cannot vote before the voting session was allowed by adm
-// a voter cannot vote after the voting session is ended by admin
-// a voter can vote only once
-
-  })
+    }
+  )
